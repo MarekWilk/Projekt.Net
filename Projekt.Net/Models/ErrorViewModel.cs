4b4fe3322390ae20fb7c17d18b92be511e617d8d
MarekWilk <maro_will@wp.pl>
2023-01-06 23:33:26 +0100

    Dodaj pliki projektów.

---
+++ Projekt.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
