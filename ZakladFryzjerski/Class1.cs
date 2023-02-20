44beec0ad7bc1e3e7584f420dca762cad6763936
MarekWilk <maro_will@wp.pl>
2023-02-21 00:28:07 +0100

    Add files via upload

---
+++ ZakladFryzjerski/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ZakladFryzjerski.Model;
+
+namespace ZakladFryzjerski
+{
+    public class Class1
+    {
+        myappEntities obj = new myappEntities();
+    }
+}
